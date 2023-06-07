ccbfb8a884f778bb023731ebe65e72fe075d274f
iisyos <kosuke1216a@yahoo.co.jp>
2023-06-07 23:07:06 +0900

    update aws_iam_role_policy_attachment

--- terraform/user_resources/modules/ecs/main.tf
+++ terraform/user_resources/modules/ecs/main.tf
@@ -17,6 +17,11 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_iam_role" "ecs_task_execution_role" {
   name               = "MyEcsTaskRole"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
